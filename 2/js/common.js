710d0ea54a8cd0f2e6835510b3db5c32e9d15d02
splDead <spldead@gmail.com>
2016-12-11 19:20:09 +0300

    Сделал второе ДЗ

---
+++ 2/js/common.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.owl-carousel').owlCarousel({
+        animateOut: 'fadeOutDown',
+        animateIn: 'fadeInDown',
+        items:1,
+        loop:true,
+        center:true,
+        smartSpeed:450
+    })
+});
