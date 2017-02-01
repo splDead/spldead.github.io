b15d6e146d96fa2e857f41807034994d13803a96
splDead <spldead@gmail.com>
2017-02-01 23:50:47 +0300

    Доработал адаптивность

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-$(function(){$(".menu").on("click",function(){$(".navigation__list").toggleClass("dropdown"),$(".hamburger--squeeze").toggleClass("is-active")})});
+$(function(){$(".menu").on("click",function(){$(this).toggleClass("active-menu"),$(".navigation__list").toggleClass("dropdown"),$(".hamburger--squeeze").toggleClass("is-active")})});
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 $(function () {
     $(".menu").on("click", function () {
+        $(this).toggleClass("active-menu");
         $(".navigation__list").toggleClass("dropdown");
         $(".hamburger--squeeze").toggleClass("is-active");
     })
