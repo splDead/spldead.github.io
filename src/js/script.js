aeb9015452f21759be9df72c503505172a4c51f2
splDead <spldead@gmail.com>
2017-02-03 01:17:06 +0300

    Для индекс страницы сделал картинки кликабельными. Добавил страницу резюме

--- src/js/script.js
+++ src/js/script.js
@@ -3,5 +3,25 @@ $(function () {
         $(this).toggleClass("active-menu");
         $(".navigation__list").toggleClass("dropdown");
         $(".hamburger--squeeze").toggleClass("is-active");
-    })
+    });
+
+
+    $(".certificates__image > img").on("click", function () {
+
+        if ($(window).width() <= 600) {
+            return;
+        }
+
+        var img = $(this),
+            over = $(".overlay");
+
+        over.fadeToggle();
+        over.on('click', function () {
+            img.removeClass("modal");
+            $(this).fadeOut();
+        });
+
+        img.toggleClass("modal");
+    });
+
 });
